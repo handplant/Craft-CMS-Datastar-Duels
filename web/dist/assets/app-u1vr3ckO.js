c36118d54f85ed23372e009faea5e77b84a7a39b
Andreas Grether <handplant@users.noreply.github.com>
2025-05-30 08:11:32 +0200

    Add leader and rival classes to duel links based on vote counts

--- web/dist/assets/app-u1vr3ckO.js
+++ web/dist/assets/app-u1vr3ckO.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".ds-cloak").forEach(e=>{e.classList.remove("ds-cloak")})});
-//# sourceMappingURL=app-pK8V9l_A.js.map
+//# sourceMappingURL=app-u1vr3ckO.js.map
