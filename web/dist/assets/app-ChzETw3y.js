0a1097ca4d73f839e6104de71364e05f6efe2bae
Andreas Grether <handplant@users.noreply.github.com>
2025-05-30 08:20:24 +0200

    Update duel display and voting logic

--- web/dist/assets/app-ChzETw3y.js
+++ web/dist/assets/app-ChzETw3y.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".ds-cloak").forEach(e=>{e.classList.remove("ds-cloak")})});
-//# sourceMappingURL=app-u1vr3ckO.js.map
+//# sourceMappingURL=app-ChzETw3y.js.map
